56352217ad78fe2a5fc90ab2f34f4d1068360bdf
davidssm97 <dssalazar@tcc.com.co>
2024-08-17 16:43:39 -0500

    creamos el modelo de productos

---
+++ src/modells/productoModel.js
@@ -0,0 +1,9 @@
+export class Product {
+    constructor(id, nombre, descripcion, precio, categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+}
