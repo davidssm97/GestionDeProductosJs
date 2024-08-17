3947c2b9a2f1483db723f201e6533aa0e65c3f27
davidssm97 <dssalazar@tcc.com.co>
2024-08-17 16:42:12 -0500

    creamos el modelo de la informacion del usuario

---
+++ src/modells/userModel.js
@@ -0,0 +1,11 @@
+class Usuario {
+    constructor(nombre, email, password, rol) {
+      this.nombre = nombre;
+      this.email = email;
+      this.password = password;
+      this.rol = rol;
+    }
+  }
+  
+  export default Usuario;
+  
